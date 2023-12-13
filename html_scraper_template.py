e9d1f071299ff35e432816cfe89ff1335f7879c2
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-12-13 13:31:24 +0200

    Add basic finders

--- html_scraper_template.py
+++ html_scraper_template.py
@@ -17,3 +17,23 @@ print('--------------------------------')
 print(page.content)
 
 soup = BeautifulSoup(page.text, 'html.parser')
+
+
+# get all <h1> elements 
+# on the page
+h1_elements = soup.find_all('h1')
+print('--------------------------------')
+print(h1_elements)
+
+# get the element with id="main-title"
+main_title_element = soup.find(id='main-title')
+print('--------------------------------')
+print(main_title_element)
+
+# find the email input element
+# through its "name" attribute
+# email_element = soup.find(attrs={'name': 'email'})
+
+# find all the centered elements
+# on the page
+# centered_element = soup.find_all(class_='text-center')
